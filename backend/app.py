cdc1c19637963458351022f5a73c2552689f7e00
Ibrahim Olanigan <salalite@gmail.com>
2024-11-13 14:49:04 -0500

    middleware template

---
+++ backend/app.py
@@ -0,0 +1,17 @@
+#
+
+
+def get_software(query: str):
+    # Query KG for related software
+    # Process result into UI schema
+    # return result
+
+    return []
+
+def get_version(software: str):
+
+    return [] # List of versions
+
+
+def SBOM_Service(name: str, version: str):
+    return []
