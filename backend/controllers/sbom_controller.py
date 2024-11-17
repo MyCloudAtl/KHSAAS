b2f7f7390df6c3780b963e6fb3817f31a0c66bae
MyCloudAtl <megandmashburn@gmail.com>
2024-11-17 16:23:43 -0500

    ReadMe edits and localhost change

--- backend/controllers/sbom_controller.py
+++ backend/controllers/sbom_controller.py
@@ -2,7 +2,7 @@ from flask import request, jsonify
 from services.sbom_service import SBOMService
 from utils.sparql_client import SPARQLClient
 
-sbom_service =SBOMService(SPARQLClient('http://localhost:3030/kg/', 'http://localhost:3030/dataset/update'))
+sbom_service =SBOMService(SPARQLClient('http://localhost:4040/kg/', 'http://localhost:4040/dataset/update'))
 
 def get_sbom_controller():
     data = request.get_json()
