6d8fc96bdd4b53dfe2a0e5d5f43867216a37c6cc
PGMoDE <gerry.murphy@pgmode.com>
2024-11-17 14:17:41 -0700

    replicate master

--- backend/models/relation_extraction_model.py
+++ backend/models/relation_extraction_model.py
@@ -1,9 +1,8 @@
 def extract_relations(ner_results):
-    # Aquí iría la lógica para extraer relaciones de los resultados NER
-    # Por simplicidad, devolvemos un resultado simulado
-    return {
-        'relations': [
-            { 'source': ner_results['entities'][0]['value'], 'target': 'Dependency', 'relation': 'dependsOn' },
-            # Agregar más relaciones según sea necesario
-        ]
-    }
+
+    relations = []
+    for entity in ner_results:
+        if entity["entity"] == "Software":
+
+            relations.append({'source': entity['value'], 'target': 'Dependency', 'relation': 'dependsOn'})
+    return {'relations': relations}
